be8d31714932bee6b5b8db2e7fa5a80c3408f58d
MordarskiK <102521828+MordarskiK@users.noreply.github.com>
2023-11-27 09:11:21 +0100

    lesson7

---
+++ lesson7.php
@@ -0,0 +1,32 @@
+<?php
+
+touch("file1.txt");
+
+//fopen("fileName",'type') type: r(read) / w(write) / a(append) 
+
+$temp = fopen("file1.txt","r");
+var_dump($temp);
+
+fclose($temp);
+
+
+fopen("file2.txt","r"); //nope 
+fopen("file2.txt","w"); //ok
+
+echo "<br>";
+
+var_dump(file_exists("file3.txt"));
+
+echo "<br>";
+
+var_dump(is_file('file2.txt'));
+
+
+
+touch("file3.txt");
+$file3 = fopen("file3.txt","w");
+fwrite($file3,"Hello World");
+fclose($file3);
+
+
+?>
