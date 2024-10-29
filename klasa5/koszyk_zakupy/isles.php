a1d364ba2c978b238c959c4503fa4164a1282ebb
MordarskiK <102521828+MordarskiK@users.noreply.github.com>
2024-10-29 12:19:23 +0100

    TBC 29-10-2024

---
+++ klasa5/koszyk_zakupy/isles.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="checkout.php" method="post">
+        <p>Mleko: <input type="checkbox" name="stuff"></p>
+        <p>Płatki: <input type="checkbox" name="stuff"></p>
+        <p>Woda: <input type="checkbox" name="stuff"></p>
+        <p>Jabłko: <input type="checkbox" name="stuff"></p>
+        <p>Banan: <input type="checkbox" name="stuff"></p>
+        <p>Pomarańcza: <input type="checkbox" name="stuff"></p>
+        <input type="submit" value="Zapłać">
+    </form>
+</body>
+</html>
---
+++ klasa5/koszyk_zakupy/store.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Koszyk Zakupowy</title>
+</head>
+<body>
+    <p>Witamy w naszym sklepie.</p>
+    <p>Wybierz to co ci sie podoba</p>
+    <a href="isles.php">Wybierz</a>
+</body>
+</html>
