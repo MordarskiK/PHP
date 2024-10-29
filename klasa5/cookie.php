a17830c267df81dc43209634b4eca20141f0566c
MordarskiK <102521828+MordarskiK@users.noreply.github.com>
2024-10-29 11:05:27 +0100

    Update cookie.php

--- klasa5/cookie.php
+++ klasa5/cookie.php
@@ -42,3 +42,40 @@ if(!isset($_COOKIE["dane"]) && !isset($_POST['nazwa'])){
 }   
     ?>
     </html>
+
+////////////////////////////////////////////////////
+/* Update z 29-10-2024 */
+
+<?php
+
+if(!isset($_COOKIE['odwiedz'])){
+    $odw = 1;
+}else{
+    $odw = intval($_COOKIE['odwiedz'])+1;  /* intval() = parseInt() */
+}
+setcookie('odwiedz',$odw, time()+60*60*24*365);
+
+$czas = date("Y-m-d G:i:s");
+setcookie('wizyta', $czas, time()+3600);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Odwiedziny</title>
+</head>
+<body>
+    <?php
+        if(isset($_COOKIE['wizyta'])){
+            $ostatnia = $_COOKIE['wizyta'];
+            echo "Witamy ponownie!<br> Ostatnia raz odwiedziłeś nas: $ostatnia";
+        }else{
+            echo "Witamy na naszej stronie!";
+        }
+    ?>
+    <p><strong>Uwaga: </strong> Potrzebne moze być odświeżenie strony.</p>
+</body>
+</html>
+
+
