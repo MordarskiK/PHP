d782e224a43cc1e4ec7197142767391c417be340
MordarskiK <102521828+MordarskiK@users.noreply.github.com>
2024-11-06 12:20:40 +0100

    efetk obrazu tbc

---
+++ klasa5/Efekt obrazu/main.js
@@ -0,0 +1,32 @@
+const pszczolaImg = document.getElementById("pszczola_img");
+const pszczolaEfekt = document.getElementsByTagName("pszczola_efekt");
+const pszczolaBtn = document.getElementById("pszczola_btn");
+
+const pomaranczaImg = document.getElementById("pomarancza_img");
+const pomaranczaColorBtn = document.getElementById("pomarancza_color_btn");
+const pomaranczaBWBtn = document.getElementById("pomarancza_bw_btn");
+
+const owoceImg = document.getElementById("owoce_img");
+const owoceSlide = document.getElementById("owoce_slide");
+const owoceBtn = document.getElementById("owoce_btn");
+
+const zolwImg = document.getElementById("zolw_img");
+const zolwSlide = document.getElementById("zolw_slide");
+const zolwBtn = document.getElementById("zolw_btn");
+
+
+pszczolaBtn.addEventListener('click', ()=>{
+    console.log(pszczolaEfekt.checked)
+    // switch(pszczolaEfekt.checked){
+    //     case blurr:
+
+    //         break;
+
+    //     case sepia:
+    //         break;
+
+    //     case invert:
+    //         break;
+    // }
+})
+
