67e189415e1cf7c89efe99da5b182ce2e793f00d
MordarskiK <102521828+MordarskiK@users.noreply.github.com>
2024-11-25 10:27:58 +0100

    Add files via upload

--- klasa5/projekt6/biuro_podróży/biuro_podrozy.sql
+++ klasa5/projekt6/biuro_podróży/biuro_podrozy.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 17, 2024 at 07:30 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Lis 25, 2024 at 10:27 AM
+-- Wersja serwera: 10.4.28-MariaDB
+-- Wersja PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kraje`
+-- Struktura tabeli dla tabeli `kraje`
 --
 
 CREATE TABLE `kraje` (
@@ -41,15 +41,46 @@ INSERT INTO `kraje` (`ID_kraje`, `Nazwa`) VALUES
 (2, 'Grecja'),
 (3, 'Chorwacja');
 
+-- --------------------------------------------------------
+
 --
--- Indexes for dumped tables
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `pass` varchar(15) DEFAULT NULL,
+  `e_mail` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id_user`, `name`, `pass`, `e_mail`) VALUES
+(1, 'admin', 'admin', ''),
+(2, 'guest', '', ''),
+(3, 'temp_user', 'temp_pass', 'temp@gmail.com'),
+(4, '', '', ''),
+(5, 'temp', 'temp_pass', 'temp@gmail.com');
+
+--
+-- Indeksy dla zrzutów tabel
 --
 
 --
--- Indexes for table `kraje`
+-- Indeksy dla tabeli `kraje`
 --
 ALTER TABLE `kraje`
   ADD PRIMARY KEY (`ID_kraje`);
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -60,6 +91,12 @@ ALTER TABLE `kraje`
 --
 ALTER TABLE `kraje`
   MODIFY `ID_kraje` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
